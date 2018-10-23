2267a16ddc7678bf86cb3c9959511b49a8545ef5
hamzaahmed742 <hamza.ahmed742@gmail.com>
2018-10-23 15:43:02 +0500

    bug fix in build script

--- bin/build.sh
+++ bin/build.sh
@@ -11,7 +11,7 @@ export GOPATH=/opt/loopring/go-src
 sudo cp -rf $WORK_DIR/src/bin/svc/* $SVC_DIR
 sudo chmod -R 755 $SVC_DIR
 
-SRC_DIR=$GOPATH/src/github.com/Hamzaahmed742/relay_cluster
+SRC_DIR=$GOPATH/src/github.com/expanse-org/relay_cluster
 if [ ! -d $SRC_DIR ]; then
       sudo mkdir -p $SRC_DIR
 	  sudo chown -R ubuntu:ubuntu $GOPATH
@@ -21,6 +21,7 @@ cd $SRC_DIR
 rm -rf ./*
 cp -r $WORK_DIR/src/* ./
 go get -u github.com/shopify/sarama
+go get -u github.com/expanse-org/relay-lib
 go build -ldflags -s -v  -o build/bin/relay cmd/main.go
 echo "go build finished......."
 cp build/bin/relay $WORK_DIR/bin
